091638a3413a1b488fa3d014696ea915f71cd84b
Mikel Gascon <mikelgascon@outlook.com>
2023-03-31 15:16:51 -0400

    add search bar

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { deleteProduct } from '../api/delete-product';
 import { Header } from '../components/header';
 import { Container } from '../components/container';
+import { useState } from 'react';
 
 const ProductsContainer = styled.div({
   display: 'flex',
@@ -24,7 +25,7 @@ const StyledLink = styled(Link)({
   cursor: 'pointer',
   padding: '0.5em 1em',
   border: '1px solid #aeaeae',
-  margin: '1em',
+  margin: '1em 1em 0',
   textDecoration: 'none',
   transition: 'color 0.1s ease-in-out, background 0.1s ease-in-out',
   '&:hover': {
@@ -33,10 +34,71 @@ const StyledLink = styled(Link)({
   },
 });
 
+const SearchBar = styled.input({
+  background: 'none',
+  borderRadius: '3em',
+  color: '#fff',
+  cursor: 'pointer',
+  padding: '0.5em 1em',
+  border: '1px solid #aeaeae',
+  fontSize: '1em',
+});
+
+const Flex = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  gap: '3em',
+  margin: '1em 0',
+  padding: '0 1em',
+});
+
+const StyledButton = styled.button(({ activeBtn }) => ({
+  background: 'none',
+  borderRadius: '3em',
+  color: '#fff',
+  cursor: 'pointer',
+  padding: '0.5em 1em',
+  border: '1px solid #aeaeae',
+  fontSize: '1em',
+  transition: 'color 0.1s ease-in-out, background 0.1s ease-in-out',
+
+  ...(activeBtn && {
+    background: '#fff',
+    color: '#4c4c4e',
+  }),
+}));
+
+const BtnContainer = styled.div({
+  display: 'flex',
+  gap: '0.5em',
+});
+
 export const Products = () => {
   const { products, setProducts } = useContext(ProductContext);
-
   const nav = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [activeBtn, setActiveBtn] = useState('scrumMaster');
+
+  const searchResults = products.filter((product) => {
+    if (!searchTerm) {
+      return product;
+    }
+
+    if (activeBtn === 'scrumMaster') {
+      return product.scrumMasterName
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+    } else {
+      return product.developers.some((developer) =>
+        developer.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+  });
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   const onEditClick = (productId) => {
     nav(`/edit-product/${productId}`);
@@ -52,7 +114,11 @@ export const Products = () => {
     setProducts(newProducts);
   };
 
-  const productList = products?.map((product) => (
+  const handleOnActiveClick = (activeBtn) => {
+    setActiveBtn(activeBtn);
+  };
+
+  const productList = searchResults?.map((product) => (
     <Product
       key={product.productId}
       product={product}
@@ -63,7 +129,33 @@ export const Products = () => {
 
   return (
     <Container>
-      <Header title="Products" count={products.length} />
+      <Header title="Products" count={searchResults.length} />
+      <Flex>
+        <SearchBar
+          type="text"
+          placeholder="Search by..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <BtnContainer>
+          <StyledButton
+            activeBtn={activeBtn === 'scrumMaster'}
+            onClick={() => {
+              handleOnActiveClick('scrumMaster');
+            }}
+          >
+            Scrum Master
+          </StyledButton>
+          <StyledButton
+            activeBtn={activeBtn === 'developers'}
+            onClick={() => {
+              handleOnActiveClick('developers');
+            }}
+          >
+            Developer
+          </StyledButton>
+        </BtnContainer>
+      </Flex>
       <StyledLink to="/new-product">Add a new product</StyledLink>
       {products.length > 0 && (
         <ProductsContainer>{productList}</ProductsContainer>
